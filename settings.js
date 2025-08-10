d8f4bf0cfd6f02e7143a57d8fe68ee1f80f997fa
UsmanabdullahiD <dalwa.uad@gmail.com>
2025-08-10 16:58:38 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const settings = {
   botOwner: 'UA_DALWA', // Your name
   ownerNumber: '2349017381024', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.8",
 };
